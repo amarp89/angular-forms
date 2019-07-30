626461dd603ee1bc0c5139a39c405a2569afefb7
Amar Patil <ampa1018@netcracker.com>
2019-07-30 14:26:04 +0530

    components creation

---
+++ angular-forms-project/src/app/dynamic-reactive-form/dynamic-reactive-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-reactive-form',
+  templateUrl: './dynamic-reactive-form.component.html',
+  styleUrls: ['./dynamic-reactive-form.component.less']
+})
+export class DynamicReactiveFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
